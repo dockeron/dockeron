7b774472c98b8743cc691c0e88921ec6588fef88
luckylin <luckylin2014@gmail.com>
2017-03-31 21:43:00 +0200

    fix error left by merge

--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -10,15 +10,9 @@ export default [
     component: require('components/HomePageView/SingleContainerView')
   },
   {
-<<<<<<< HEAD
-    path: '/containers',
-    name: 'containers-view',
-    component: require('components/HomePageView/ContainersView')
-=======
     path: '/images',
     name: 'images-view',
     component: require('components/HomePageView/ImagesView')
->>>>>>> master
   },
   {
     path: '*',
