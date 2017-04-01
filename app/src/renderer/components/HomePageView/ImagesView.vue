8cbec5b11e3c0a8f0e36503fc7aae1b0f5410a49
luckylin <luckylin2014@gmail.com>
2017-04-01 16:24:51 +0200

    add simple image-view

--- app/src/renderer/components/HomePageView/ImagesView.vue
+++ app/src/renderer/components/HomePageView/ImagesView.vue
@@ -2,167 +2,111 @@
   <div>
     <Button
         type="primary"
-        :loading="loadingContainers"
+        :loading="loadingImages"
         icon="ios-loop-strong"
-        @click="refreshContainers"
+        @click="refreshImages"
         class="refresh-button">
-      <span v-if="!loadingContainers">Refresh</span>
+      <span v-if="!loadingImages">Refresh</span>
       <span v-else>Loading...</span>
     </Button>
     <br>
-    <div v-if="hasFoundContainers">
-      <Card v-for="c in containers" class="container-card">
-        <p slot="title" class="container-card-title">
-          Container: {{c.Names[0]}}
-          <Tag :color="stateToColor[c.State]" class="container-state-tag">{{c.State}}</Tag>
+    <div v-if="hasFoundImages">
+      <Card v-for="image in images" class="image-card">
+        <p slot="title" class="image-card-title">
+          Images: {{getImageName(image.RepoTags[0])}}
         </p>
-        <p>Image: {{c.Image}}</p>
-        <p>Status: {{c.Status}}</p>
-        <Button type="primary" @click="moreIsOpened = true">More</Button>
-        <Button type="warning" @click="inspectContainer(c.Id)">Inspect</Button>
-        <container-control-panel
-            :container-id="c.Id"
-            @container-data-refreshed="function (newData) { loadContainers() }"
-            class="control-panel">
-        </container-control-panel>
-        <Modal
-          v-model="moreIsOpened"
-          :title="c.Names[0]"
-          @on-ok="ok"
-          @on-cancel="cancel">
-            <pre>{{c}}</pre>
-        </Modal>
+        Tags: <Tag v-for="tag in getTags(image.RepoTags)">{{tag}}</Tag>
+        <p> Size: {{formatBytes(image.Size)}}</p>
+        <p> Created: {{getDateTime(image.Created)}}</p>
       </Card>
     </div>
     <div v-else>
-      <h4>No containers found.</h4>
+      <h4>No images found.</h4>
       <pre>{{error}}</pre>
     </div>
   </div>
 </template>
 
 <script>
-  import ContainerControlPanel from './ContainerControlPanel'
-
   import docker from '../../js/docker'
 
   export default {
-    components: {
-      ContainerControlPanel
-    },
     data () {
       return {
-        containers: [],
-        hasFoundContainers: false,
+        images: [],
+        hasFoundImages: false,
         error: '',
-        stateToColor: {
-          created: 'blue',
-          restarting: 'yellow',
-          running: 'green',
-          paused: 'yellow',
-          exited: 'red',
-          dead: 'red'
-        },
-        moreIsOpened: false,
-        loadingContainers: false
+        loadingImages: false
       }
     },
     watch: {
-      containers: function (newContainers) {
-        this.hasFoundContainers = typeof newContainers !== 'undefined' && newContainers !== null && newContainers.length > 0
+      images: function (newImages) {
+        this.hasFoundImages = typeof newImages !== 'undefined' && newImages !== null && newImages.length > 0
       }
     },
     methods: {
-      ok () {
-        console.log('Open More.')
+      refreshImages () {
+        // this.info('Start loading images.')
+        this.loadingImages = true
+        this.loadImages()
+        // this.info('Finish loading images.')
+        this.loadingImages = false
       },
-      cancel () {
-        console.log('Close More.')
-      },
-      refreshContainers () {
-        // this.info('Start loading containers.')
-        this.loadingContainers = true
-        this.loadContainers()
-        // this.info('Finish loading containers.')
-        this.loadingContainers = false
-      },
-      inspectContainer (containerId) {
-        console.log('Inspect: ', containerId)
-        this.$router.push({
-          name: 'single-container-view',
-          params: { containerId: containerId }
-        })
-      },
-      info (content) {
-        this.$Notice.info({
-          title: content,
-          desc: ''
-        })
-      },
-      success (content) {
-        this.$Notice.success({
-          title: content,
-          desc: ''
-        })
-      },
-      warning (content) {
-        this.$Notice.warning({
-          title: content,
-          desc: ''
-        })
-      },
-      error (content) {
-        this.$Notice.error({
-          title: content,
-          desc: ''
-        })
-      },
-      loadContainers () {
+      loadImages () {
         var self = this
-
         var queries = {
           all: true,
           size: true
         }
-
-        docker.listContainers(queries, function (err, containers) {
+        docker.listImages(queries, function (err, images) {
           if (!err) {
-            console.log(containers)
+            console.log(images)
           } else {
             console.log(err)
           }
-          self.containers = containers
+          self.images = images
           self.error = err
         })
+      },
+      getImageName (repoTag) {
+        return repoTag.slice(0, repoTag.indexOf(':'))
+      },
+      getTags (repoTags) {
+        var tags = []
+        repoTags.forEach(function (repoTag) {
+          tags.push(repoTag.slice(repoTag.indexOf(':') + 1))
+        })
+        return tags
+      },
+      formatBytes (bytes) {
+        if (bytes === 0) return '0 Bytes'
+        const k = 1000
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        var i = Math.floor(Math.log(bytes) / Math.log(k))
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
+      },
+      getDateTime (seconds) {
+        return new Date(seconds * 1000).toLocaleString()
       }
     },
     created () {
-      this.loadContainers()
+      this.loadImages()
     }
   }
 </script>
 
 <style scoped>
-  .container-card {
+  .image-card {
     width: 300px;
     display: inline-block;
     margin-right: 10px;
   }
 
-  .container-card-title {
+  .image-card-title {
     height: 26px;
   }
 
   .refresh-button {
     display: block;
-  }
-
-  .control-panel {
-    display: inline-block;
-  }
-
-  .container-state-tag {
-    position: absolute;
-    right: 3px;
   }
 </style>
