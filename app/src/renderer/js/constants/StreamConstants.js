19129af91824f688142ad77c53392bba01255100
fluency03 <fluency.03@gmail.com>
2017-07-22 19:43:55 +0200

    add stream constants

---
+++ app/src/renderer/js/constants/StreamConstants.js
@@ -0,0 +1,14 @@
+// stream writable
+export const STREAM_WRITABLE_EVENT_CLOSE = 'close'
+export const STREAM_WRITABLE_EVENT_DRAIN = 'drain'
+export const STREAM_WRITABLE_EVENT_ERROR = 'error'
+export const STREAM_WRITABLE_EVENT_FINISH = 'finish'
+export const STREAM_WRITABLE_EVENT_PIPE = 'pipe'
+export const STREAM_WRITABLE_EVENT_UNPIPE = 'unpipe'
+
+// stream readable
+export const STREAM_READABLE_EVENT_CLOSE = 'close'
+export const STREAM_READABLE_EVENT_DATA = 'data'
+export const STREAM_READABLE_EVENT_END = 'end'
+export const STREAM_READABLE_EVENT_ERROR = 'error'
+export const STREAM_READABLE_EVENT_READABLE = 'readable'
