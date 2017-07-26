4484a869e4d1045e815c07e9d7c59d1f21257a3a
fluency03 <fluency.03@gmail.com>
2017-07-26 16:41:52 +0200

    add eslint: template-curly-spacing: [2, never], no-useless-escape: 2, wrap-iife: [2, outside], no-loop-func: 2, prefer-rest-params: 2, prefer-spread: 2, no-new-func: 2, no-param-reassign: [2, { props: true }], prefer-arrow-callback: 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,11 +25,21 @@ module.exports = {
     // no console
     'no-console': 2,
     'no-new-object': 2,
-    'quote-props': ["error", "as-needed"],
-    "object-shorthand": ["error", "always"],
+    'quote-props': [2, 'as-needed'],
+    'object-shorthand': [2, 'always'],
     'no-array-constructor': 2,
     'array-callback-return': 2,
     'prefer-template': 2,
+    'template-curly-spacing': [2, 'never'],
+    'no-useless-escape': 2,
+    // 'func-style': [2, 'expression'],
+    'wrap-iife': [2, 'outside'],
+    'no-loop-func': 2,
+    'prefer-rest-params': 2,
+    'prefer-spread': 2,
+    'no-new-func': 2,
+    'no-param-reassign': [2, { 'props': true }],
+    'prefer-arrow-callback': 2,
     // not assign to const
     'no-const-assign': 2
     // no var, use let and const
