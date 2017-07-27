0331a187cb2813d4396fd8e771a89db568469ae0
fluency03 <fluency.03@gmail.com>
2017-07-27 12:22:43 +0200

    setIPCChannels to upper case

---
+++ src/main/setIPCChannels.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import { ipcMain, dialog } from 'electron'
+import {
+  IPC_CHANNEL_OPEN_FILE_DIALOG,
+  IPC_CHANNEL_SELECTED_DIRECTORY,
+  IPC_CHANNEL_OPEN_SAVE_DIALOG,
+  IPC_CHANNEL_SAVED_FILE
+} from '../renderer/js/constants/ElectronConstants'
+
+export default function setIPCChannels () {
+  ipcMain.on(IPC_CHANNEL_OPEN_FILE_DIALOG, event => {
+    dialog.showOpenDialog({
+      properties: [
+        'openFile',
+        'openDirectory'
+      ]
+    }, files => {
+      if (files) {
+        event.sender.send(IPC_CHANNEL_SELECTED_DIRECTORY, files)
+      }
+    })
+  })
+
+  ipcMain.on(IPC_CHANNEL_OPEN_SAVE_DIALOG, (event, path) => {
+    const options = {
+      title: 'Save File',
+      defaultPath: path
+    }
+
+    dialog.showSaveDialog(options, filename => {
+      event.sender.send(IPC_CHANNEL_SAVED_FILE, filename)
+    })
+  })
+}
